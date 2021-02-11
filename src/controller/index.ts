7ba5d4e44148305270d40a0d13914bc1ee660b7e
caldric <cvaldric@gmail.com>
2021-02-11 12:59:57 -0500

    Add post route for creating new links

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express'
+import Link from '../model'
 
 const router = express.Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello world')
+router.post('/', async (req: Request, res: Response) => {
+  // Obtain URL
+  const { url } = req.body
+
+  // Create new URL in the database
+  const newUrl = await Link.create({ url }).catch((err) =>
+    res.status(500).json({ error: err.message })
+  )
+  return res.status(200).json(newUrl)
 })
 
 export default router
